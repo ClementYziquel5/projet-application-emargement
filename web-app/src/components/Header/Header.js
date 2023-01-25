2766d043748167cb8e93c45827c78f0e4eaba7db
cyziqu24 <clement.yziquel@isen-ouest.yncrea.fr>
2023-01-25 20:05:56 +0100

    Création composant Header

---
+++ web-app/src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import './Header.css'
+
+function Header(){
+    return (
+        <div className="header">
+             <div className="logo-div">
+                <img src="logo.png" className="logo" alt='ISEN Logo'></img>
+            </div>
+            <div className="navbar">
+                <p className="navbar-element">Sessions</p>
+                <p className="navbar-element">Groupes</p>
+                <p className="navbar-element">Création</p>
+            </div>
+            <div className="hamburger-div">
+                <img src="hamburger-blanc.png" className="hamburger" alt='hamburger'></img>
+            </div>
+            
+        </div>
+       
+    );
+}
+
+export default Header;
