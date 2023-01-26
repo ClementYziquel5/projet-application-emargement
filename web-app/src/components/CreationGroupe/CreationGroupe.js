fa802463e31de839507878ee233f914e6973e386
cyziqu24 <clement.yziquel@isen-ouest.yncrea.fr>
2023-01-26 15:36:59 +0100

    Avancement CreationGroupe

--- web-app/src/components/CreationGroupe/CreationGroupe.js
+++ web-app/src/components/CreationGroupe/CreationGroupe.js
@@ -5,29 +5,38 @@ function CreationGroupe(props){
         <div>
             <div className='creation-groupe'>
                 
-                <div className='T1'>
+                <div className='inputs'>
                     <div>
                         <label htmlFor='nomGroupe'>Nom du groupe</label>
                         <input name='nomGroupe' id='nomGroupe' type='text'></input>
                     </div>
 
-                    <div className='T2'>
-                        <form>
-                            <div>
-                                <label htmlFor='nom'>Nom</label>
-                                <input name='nom' id='nom' type='text'></input>
-                            </div>
-                            <div>
-                                <label htmlFor='prenom'>Prenom</label>
-                                <input name='prenom' id='prenom' type='text'></input>
-                            </div>
+                    <div className='input-eleve'>
+                        <div className='Prénom'>
+                            <label htmlFor='nom'>Nom</label>
+                            <input name='nom' id='nom' type='text'></input>
+                        </div>
+                        <div>
+                            <label htmlFor='prenom'>Prenom</label>
+                            <input name='prenom' id='prenom' type='text'></input>
+                        </div>
 
-                            <input type='submit' value='AJOUTER ➤'></input>
-                        </form>
+                        <button class="button-rectangle" type="button">AJOUTER ➤</button>
+
                     </div>
 
                 </div>
 
+                <div className='liste'>
+                    <h3>Liste des élèves</h3>
+                    <div className='eleves'>
+                        <div className='eleve'>
+                            <img src='Supprimer.png' alt='Supprimer'></img>
+                            <p>NOM Prénom</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="button-rectangle" type="button">Afficher</button>
             </div>
         </div>
     );
