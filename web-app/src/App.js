629964bec419aba008dfa11f9d2635845f6013c9
cyziqu24 <clement.yziquel@isen-ouest.yncrea.fr>
2023-01-31 14:56:01 +0100

    Ajout de la navigation

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,17 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
 import Header from './components/Header/Header.js';
 import Filtres from './components/Filtres/Filtres.js';
 import Listes from './components/Listes/Listes.js';
-import Top from './components/Top/Top.js';
 import CreationSession from './components/CreationSession/CreationSession.js';
+import CreationGroupe from './components/CreationGroupe/CreationGroupe.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Top/>
-      <Listes/>
-    </div>
-  );
-}
+const App = () => (
+    <Router>
+        <div>
+            <Header/>
+            <Routes>
+                <Route exact path="/sessions" element={<Sessions/>} />
+                <Route path="/groupes" element={<Groupes/>} />
+                <Route path="/creation" element={<Creation/>} />
+            </Routes>
+        </div>
+    </Router>
+);
+
+const Sessions = () => (
+  <div>
+    <Filtres/>
+    <Listes/>
+  </div>
+);
+const Groupes = () => (
+  <div>
+    <Filtres/>
+  </div>
+);
+const Creation = () => (
+  <div>
+    <CreationSession/>
+    <CreationGroupe/>
+  </div>
+);
 
 export default App;
