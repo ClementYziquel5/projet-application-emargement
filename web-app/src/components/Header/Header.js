f495f39bf96835025ae67a99dec58fe80ff991c2
yohannlc <yohann.lecamx@gmail.com>
2023-01-31 10:04:54 +0100

    Amelioration page filtres/affichage

--- web-app/src/components/Header/Header.js
+++ web-app/src/components/Header/Header.js
@@ -2,19 +2,21 @@ import './Header.css'
 
 function Header(){
     return (
-        <div className="header">
-             <div className="logo-div">
-                <img src="logo.png" className="logo" alt='ISEN Logo'></img>
+        <div className="top-header">
+            <div className="header">
+                <div className="logo-div">
+                    <img src="logo.png" className="logo" alt='ISEN Logo'></img>
+                </div>
+                <div className="navbar">
+                    <p className="navbar-element">Sessions</p>
+                    <p className="navbar-element">Groupes</p>
+                    <p className="navbar-element">Création</p>
+                </div>
+                <div className="hamburger-div">
+                    <img src="hamburger-blanc.png" className="hamburger" alt='hamburger'></img>
+                </div>
+                
             </div>
-            <div className="navbar">
-                <p className="navbar-element">Sessions</p>
-                <p className="navbar-element">Groupes</p>
-                <p className="navbar-element">Création</p>
-            </div>
-            <div className="hamburger-div">
-                <img src="hamburger-blanc.png" className="hamburger" alt='hamburger'></img>
-            </div>
-            
         </div>
        
     );
