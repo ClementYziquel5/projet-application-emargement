8aaea1d2d9df9bbef8bc722beabb0dfce1166059
cyziqu24 <clement.yziquel@isen-ouest.yncrea.fr>
2023-02-01 09:47:06 +0100

    Réparer routage

--- web-app/src/components/Header/Header.js
+++ web-app/src/components/Header/Header.js
@@ -1,33 +1,37 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 function Header(){
-    const [showInfos, setShowInfos] = useState(true);
+    const [showInfos, setShowInfos] = useState(false);
     const handleClick = () => {
       setShowInfos(!showInfos);
     };
 
     return (
         <div className="header">
-             <div className="logo-div">
+            <div className="logo-div">
                 <img src="logo.png" className="logo" alt='ISEN Logo'></img>
             </div>
+
             <div className="navbar">
-                <p className="navbar-element">Sessions</p>
-                <p className="navbar-element">Groupes</p>
-                <p className="navbar-element">Création</p>
+                <Link to="/sessions">Sessions</Link>
+                <Link to="/groupes">Groupes</Link>
+                <Link to="/creation">Création</Link>
             </div>
-            <div className="hamburger-div">
-                <img src="hamburger-blanc.png" className="hamburger" alt='hamburger'  onClick={handleClick}></img>
-                
+
+            <div>
+                <div className="hamburger-div">
+                    <img src="hamburger-blanc.png" className="hamburger" alt='hamburger'  onClick={handleClick}></img>             
+                </div>
+            
+                <div className='infos-hamburger' style={{ display: showInfos ? 'block' : 'none' }}>
+                    <p id='nom'>Nom Prenom</p>
+                    <p id='pseudo'>pseudo</p>
+                    <a href='#'><p className='deconnexion'>Déconnexion</p></a>
+                </div>  
             </div>
-            <div className='infos-hamburger' style={{ display: showInfos ? 'block' : 'none' }}>
-                <p id='nom'>Nom Prenom</p>
-                <p id='pseudo'>pseudo</p>
-                <a href='#'><p className='deconnexion'>Déconnexion</p></a>
-            </div>    
         </div>
-       
     );
 }
 
