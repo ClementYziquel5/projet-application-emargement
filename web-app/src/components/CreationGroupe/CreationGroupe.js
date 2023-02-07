3d35bae51f3563e91fc43454d7736db3b1a7f060
yohannlc <yohann.lecamx@gmail.com>
2023-02-07 12:27:30 +0100

    Fonction d'ajout d'élèves à un groupe v1

--- web-app/src/components/CreationGroupe/CreationGroupe.js
+++ web-app/src/components/CreationGroupe/CreationGroupe.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import './CreationGroupe.css'
+import React from 'react';
 
 function CreationGroupe(props){
-    const eleves = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    const [nom, setNom] = useState();
+    const [prenom, setPrenom] = useState();
+    const [etudiants, setEtudiants] = useState([]);
+
+    function handleNom(e) {
+        setNom(e.target.value);
+        console.log(e.target.value);
+    }
+
+    function handlePrenom(e) {
+        setPrenom(e.target.value);
+        console.log(e.target.value);
+    }
+
+    function handleAddEtudiant() {
+        setEtudiants([...etudiants, {nom, prenom}]);
+        setNom('');
+        setPrenom('');
+    }
+    
+
     return (
         <div>
             <div className='creation-groupe'>
@@ -15,28 +38,25 @@ function CreationGroupe(props){
                     <div className='input-eleve'>
                         <div className='Prénom'>
                             <label htmlFor='nom'>Nom</label>
-                            <input name='nom' id='nom' type='text'></input>
+                            <input name='nom' id='nom' type='text' value={nom} onChange={handleNom}/>
                         </div>
                         <div>
                             <label htmlFor='prenom'>Prenom</label>
-                            <input name='prenom' id='prenom' type='text'></input>
+                            <input name='prenom' id='prenom' type='text' value={prenom} onChange={handlePrenom}/>
                         </div>
 
-                        <button class="button-rectangle" type="button">AJOUTER ➤</button>
+                        <button class="button-rectangle" type="button" onClick={handleAddEtudiant}>AJOUTER ➤</button>
 
                     </div>
                 </div>
 
                 <div className='liste'>
                     <h3>Liste des élèves</h3>
-                    <div className='eleves'>
-                        {eleves.map((eleve,index) =>
-                            <div className='eleve' id={index}>
-                                <a href='#'><img src='Supprimer.png' alt='Supprimer'></img></a>
-                                <p>NOM Prénom</p>
-                            </div>
-                        )}
-                    </div>
+                    {etudiants.map((etudiant, index) => (
+                        <div key={index}>
+                            {etudiant.nom} {etudiant.prenom}
+                        </div>
+                    ))}
                 </div>
                 <button class="button-rectangle" type="button">Créer</button>
             </div>
